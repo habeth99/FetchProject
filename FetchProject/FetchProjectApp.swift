64385a83b2eeef6f690e0443001838c6df430eb8
Nicholas Habeth <nhabethqb24@gmail.com>
2024-10-05 12:08:47 -0400

    Initial Commit

---
+++ FetchProject/FetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectApp.swift
+//  FetchProject
+//
+//  Created by Nick Habeth on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
